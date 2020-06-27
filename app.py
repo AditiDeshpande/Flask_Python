3222dbf2247281a54f20c5e09955d5cd94d1297d
aditi <aditid11@gmail.com>
2020-06-28 00:22:05 +0530

    Made some changes in string to check whether it's getting refreshed/reflected on browser on refresh.

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Aditi.. You Have Done It!!"
+    return "Hello Aditi.. You Have Done It!! Gr8! refresh is working using export FLASK_DEBUG=1"
