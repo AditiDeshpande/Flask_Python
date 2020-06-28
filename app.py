438c060274851181bcbb9be40710ef664c4ec195
aditi <aditid11@gmail.com>
2020-06-28 11:56:31 +0530

    2 Page app...

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 
+#app is a variable
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Aditi.. You Have Done It!! Gr8! refresh is working using export FLASK_DEBUG=1"
+    return "Namaskar Mandali...!!"
+
+@app.route("/aditi")
+def aditi():
+    return "Hello, Aditi! You Have Done It!! Gr8! refresh is working using export FLASK_DEBUG=1"
