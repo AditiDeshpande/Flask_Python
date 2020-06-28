32ced1792a811b2ea3d8e1147887b2d4a446e086
aditi <aditid11@gmail.com>
2020-06-28 12:13:16 +0530

    Added salutation code for any name rather any string entered. Customised route and it's function.

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ def index():
 @app.route("/aditi")
 def aditi():
     return "Hello, Aditi! You Have Done It!! Gr8! refresh is working using export FLASK_DEBUG=1"
+
+@app.route("/angad")
+def angad():
+    return "Namaskar Angad !! :) "
+
+@app.route("/<string:name>")
+def namaskar(name):
+    return f"Namaskar, {name}!"
