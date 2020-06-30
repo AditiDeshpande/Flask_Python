732ea125e4651460a3c67c7f824e8389eeda86b2
aditi <aditid11@gmail.com>
2020-07-01 00:28:46 +0530

    One Scenario explained in comment.

--- forms.py
+++ forms.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template("index_form.html")
 
+#If we directly on webpage url hit the url with /hello it gives following error
+#Method Not Allowed
+#The method is not allowed for the requested URL.
+#bcoz here we defined methods POST and when we hit url directly it is GET methods
+#so it's a mismatch that's why not allowed
 @app.route("/hello" , methods=["POST"])
 def hello():
     name = request.form.get("name")
