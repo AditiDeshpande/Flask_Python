ea82a5568a49f3ad878a9a61c6f3eb53f7b6bbf3
aditi <aditid11@gmail.com>
2020-07-02 01:36:12 +0530

    Added session to the app.

--- sessionnotes.py
+++ sessionnotes.py
@@ -7,12 +7,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-notes = []
-
 @app.route("/" , methods=["GET" , "POST"])
 def indexnotes():
+    if session.get("notes") is None:
+        session["notes"] = []
     if request.method == "POST":
         note = request.form.get("note")
-        notes.append(note)
+        #notes.append(note)
+        session["notes"].append(note)
 
-    return render_template("indexnotes.html" , notes=notes)
+    return render_template("indexnotes.html" , notes=session["notes"])
